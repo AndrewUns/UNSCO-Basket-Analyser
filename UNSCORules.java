bb5cfeb8505432d0e23986dbceeb7a18686d4681
Andrew Unsworth <112930793+AndrewUns@users.noreply.github.com>
2024-11-02 13:03:35 +0000

    Update UNSCORules.java

--- UNSCORules.java
+++ UNSCORules.java
@@ -12,19 +12,6 @@ import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Andrew Unsworth
- */
-
-
-
 public class UNSCORules extends javax.swing.JFrame {
     
     /**
